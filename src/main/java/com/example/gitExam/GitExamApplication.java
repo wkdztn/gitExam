08fb5bffa2934b033669f9c0e457706beb3e9fd5
wkdztn <wkdztn@gamil.com>
2023-07-17 12:45:55 +0900

    up

---
+++ src/main/java/com/example/gitExam/GitExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitExam/GitExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
